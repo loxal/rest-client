ffe4e54a76a4a2d1b739abd445618a537598e3b4
Alex <alexander.orlov@loxal.net>
2015-02-28 00:04:04 +0100

    use a type for headers that is closer to the Jersey API

--- src/main/kotlin/net/loxal/client/rest/model/data-declaration.kt
+++ src/main/kotlin/net/loxal/client/rest/model/data-declaration.kt
@@ -9,6 +9,7 @@ import javax.ws.rs.HttpMethod
 import java.net.URL
 import net.loxal.client.rest.App
 import java.util.Collections
+import javax.ws.rs.core.MultivaluedMap
 
 data class Header(val name: String, val value: List<Any>) {
     override public fun toString(): String {
@@ -20,7 +21,7 @@ data class RequestParameter(val paramName: String, val paramValue: Any)
 
 data class RestCode private() {
     val method: String = HttpMethod.GET
-    val headers: List<Map<String, List<Any>>> = emptyList()
+    val headers: List<MultivaluedMap<String, Any>> = emptyList()
     val body: String = ""
     val name: String = "Unnamed"
 }
@@ -28,15 +29,14 @@ data class RestCode private() {
 data class ClientRequestModel(builder: ClientRequestModel.Builder) : Serializable {
     val method: String = builder.method
     val url: URL = builder.url
-    // TODO lookup the true type of headers => MultiValueMap<String, List<String>>)?
-    val headers: List<Map<String, List<Any>>> = builder.headers
+    val headers: List<MultivaluedMap<String, Any>> = builder.headers
     val body: String = builder.body
     var name: String = builder.name
 
     public class Builder(val name: String) {
         var method: String = HttpMethod.GET
         var url: URL = App.SAMPLE_URL
-        var headers: List<Map<String, List<Any>>> = emptyList()
+        var headers: List<MultivaluedMap<String, Any>> = emptyList()
         var body: String = ""
 
         fun method(method: String): Builder {
@@ -49,7 +49,7 @@ data class ClientRequestModel(builder: ClientRequestModel.Builder) : Serializabl
             return this
         }
 
-        public fun headers(headers: List<Map<String, List<Any>>>): Builder {
+        public fun headers(headers: List<MultivaluedMap<String, Any>>): Builder {
             this.headers = headers
             return this
         }
@@ -70,8 +70,8 @@ data class ClientRequestModel(builder: ClientRequestModel.Builder) : Serializabl
         val lineBreak = "\n"
 
         // TODO unit test this
-        fun headersFromText(text: String): List<Map<String, List<Any>>> {
-            val headersFromText: List<Map<String, List<Any>>> = Collections.emptyList()
+        fun headersFromText(text: String): List<MultivaluedMap<String, Any>> {
+            val headersFromText: List<MultivaluedMap<String, Any>> = Collections.emptyList()
             if (!text.isEmpty()) {
                 text.split(lineBreak).forEach { header ->
                     if (header.contains(headerKeyValueSeparator)) {
