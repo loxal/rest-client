a36ca717c1fd1fa613463cf8213b58c070533374
Alex <alexander.orlov@loxal.net>
2015-03-05 01:21:04 +0100

    add assembly configuration; update release configuration

--- release.sh
+++ release.sh
@@ -2,3 +2,4 @@
 
 mvn clean assembly:assembly
 cp target/Epvin-v*-RELEASE-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin
+cp target/Epvin-v*-RELEASE-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin/Epvin.jar
