f247c98df9ea3723a2841491cd97d9b5ac20a9d1
Alex <alexander.orlov@loxal.net>
2015-03-06 01:59:33 +0100

    fix release scripts

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mvn clean assembly:assembly
-cp target/Epvin-v*-SNAPSHOT-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin
-cp target/Epvin-v*-SNAPSHOT-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin/Epvin.jar
+cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin
+cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin/Epvin.jar
