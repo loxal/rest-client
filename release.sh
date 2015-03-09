d24057113437ff97b86d7c6fdb1345f543dde851
Alex <alexander.orlov@loxal.net>
2015-03-10 00:07:35 +0100

    release

--- release.sh
+++ release.sh
@@ -3,4 +3,5 @@
 mvn clean assembly:assembly
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin/Epvin.jar
+
 cp target/Epvin-v*-jar-with-dependencies.jar /Applications/Epvin.app.jar
