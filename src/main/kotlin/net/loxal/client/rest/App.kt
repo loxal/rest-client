4cd94b6995f04ccfe5323f024674759dec6d06f1
Alex <alexander.orlov@loxal.net>
2015-03-18 11:33:31 +0100

    add download URL

--- src/main/kotlin/net/loxal/client/rest/App.kt
+++ src/main/kotlin/net/loxal/client/rest/App.kt
@@ -28,7 +28,7 @@ class App : Application() {
             scene = Scene(rootNode)
             stage.setScene(scene)
 
-            stage.setTitle("Epvin v${properties.getProperty("project.version")}")
+            stage.setTitle("Epvin v${properties.getProperty("project.version")} | www.loxal.net/epvin-rest-client")
             stage.getIcons().add(Image("/net/loxal/client/rest/view/tool-icon-256.png"))
             rootNode.requestFocus()
             stage.show()
