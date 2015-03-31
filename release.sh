c00a5d6a21ab9555880e436181b92930583e372d
Alex <alexander.orlov@loxal.net>
2015-03-31 04:01:58 +0200

    update release scripts

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mvn clean assembly:assembly
+mvn clean assembly:assembly -D build.number=${build.number}
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin/Epvin.jar
 
