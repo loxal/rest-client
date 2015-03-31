92fc5400eb8702a867547874b98b223dc6689b21
Alex <alexander.orlov@loxal.net>
2015-03-31 03:50:42 +0200

    fix build.number parameter

--- snapshot.sh
+++ snapshot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mvn clean assembly:assembly -Dbuild.number=%build.counter%
+mvn clean assembly:assembly -Dbuild.number=${build.number}
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin/Epvin-SNAPSHOT.jar
