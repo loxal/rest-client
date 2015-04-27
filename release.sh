f6534878c84527dc74831c5a008ce0524bc0cc00
Alex <alexander.orlov@loxal.net>
2015-04-27 21:14:25 +0200

    simplify local release app name

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ mvn clean assembly:assembly -D build.number=${build.number}
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin
 cp target/Epvin-v*-jar-with-dependencies.jar ~/Google\ Drive/public/Epvin/Epvin.jar
 
-cp target/Epvin-v*-jar-with-dependencies.jar /Applications/Epvin.app.jar
+cp target/Epvin-v*-jar-with-dependencies.jar /Applications/Epvin.jar
